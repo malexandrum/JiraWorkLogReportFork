df59c9a4258bd844c8402e2272548ec485fb8806
Alexandru MOLODOI <alexandrumm@hotmail.com>
2020-10-16 11:36:43 -0700

    window management

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
-chrome.browserAction.onClicked.addListener(function() {
-   chrome.windows.create({'url': 'index.html', 'type': 'popup'}, function(window) {
-   });
+let w;
+
+chrome.browserAction.onClicked.addListener(function () {
+   if (!w) {
+      chrome.windows.create({ url: 'index.html', type: 'popup', focused: true }, function (window) {
+         w = window      });
+   } else {      
+      chrome.windows.update(w.id, { focused: true }, function () { })
+   }
 });
+
+chrome.windows.onRemoved.addListener(function () {   
+   w = undefined;
+})
